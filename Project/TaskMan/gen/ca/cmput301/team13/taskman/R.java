b5956b84f6d49e6dd14518e41ce4ddf7e6f9efb9
Jed Barlow <jbarlow@ualberta.ca>
2012-10-24 17:24:07 -0600

    add .gitignore for unwanted java-related files

--- Project/TaskMan/gen/ca/cmput301/team13/taskman/R.java
+++ Project/TaskMan/gen/ca/cmput301/team13/taskman/R.java
@@ -10,18 +10,39 @@ package ca.cmput301.team13.taskman;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bgColor=0x7f050000;
+        public static final int bgListColor=0x7f050002;
+        public static final int bgMinorColor=0x7f050001;
+    }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int audicon=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int imgicon=0x7f020003;
+        public static final int txticon=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int alltasks_btn=0x7f080002;
+        public static final int audImg=0x7f080008;
+        public static final int audfilter_btn=0x7f080006;
+        public static final int filter_layout=0x7f080003;
+        public static final int imgImg=0x7f08000a;
+        public static final int imgfilter_btn=0x7f080005;
+        public static final int menu_settings=0x7f08000c;
+        public static final int mytasks_btn=0x7f080001;
+        public static final int strut=0x7f080000;
+        public static final int task_list=0x7f080007;
+        public static final int title_lbl=0x7f08000b;
+        public static final int txtImg=0x7f080009;
+        public static final int txtfilter_btn=0x7f080004;
     }
     public static final class layout {
         public static final int activity_root=0x7f030000;
+        public static final int task_row=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_root=0x7f060000;
+        public static final int activity_root=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -30,6 +51,6 @@ public final class R {
         public static final int title_activity_root=0x7f040003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
