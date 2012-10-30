7a28592c3d302b28b5f332c7ce7dd2743c0606c2
Taylor Lloyd <taylor@taylorlloyd.ca>
2012-10-29 22:02:46 -0600

    Preliminary TaskActivity commit

---
+++ Project/TaskMan/src/ca/cmput301/team13/taskman/TaskActivity.java
@@ -0,0 +1,20 @@
+package ca.cmput301.team13.taskman;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TaskActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_task, menu);
+        return true;
+    }
+}
