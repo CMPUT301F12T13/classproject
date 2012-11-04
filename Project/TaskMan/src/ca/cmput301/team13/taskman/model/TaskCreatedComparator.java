f032067b0c9812e9eecd46bfd92b7e5c3ea8c062
Jed Barlow <jbarlow@ualberta.ca>
2012-11-03 23:53:13 -0600

    add license header info

--- Project/TaskMan/src/ca/cmput301/team13/taskman/model/TaskCreatedComparator.java
+++ Project/TaskMan/src/ca/cmput301/team13/taskman/model/TaskCreatedComparator.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of TaskMan
+ *
+ * Copyright (C) 2012 Jed Barlow, Mark Galloway, Taylor Lloyd, Braeden Petruk
+ *
+ * TaskMan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TaskMan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TaskMan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.cmput301.team13.taskman.model;
 
 import java.util.Comparator;
