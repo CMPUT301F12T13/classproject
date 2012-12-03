e37d9934388167f67d02850b236b9bae10d09710
Jed Barlow <jbarlow@ualberta.ca>
2012-12-03 09:25:16 -0700

    add skeleton file for junit search functionality tests

---
+++ TaskMan-Tests/src/ca/cmput301/team13/taskman/test/SearchTests.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of TaskMan
+ *
+ * Copyright (C) 2012 Jed Barlow, Mark Galloway, Taylor Lloyd, Braeden Petruk
+ *
+ * TaskMan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TaskMan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TaskMan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.cmput301.team13.taskman.test;
+
+
+import ca.cmput301.team13.taskman.model.Requirement;
+import ca.cmput301.team13.taskman.model.Task;
+import ca.cmput301.team13.taskman.model.Requirement.contentType;
+
+public class SearchTests extends BaseSetup {
+    
+    public SearchTests() {
+        super();
+    }
+    
+    public void setUp() {
+        super.setUp();
+        // TODO: Add a list of tasks with various descriptions.
+    }
+    
+    public void test_search() {
+        // TODO: Try several searches and confirm proper findings.
+    }
+    
+    public void tearDown() {
+    }
+}
